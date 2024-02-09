6bfc76cbe055bcdf0d34b7d7ba0b1628803888ad
Andiea116 <Andiea116@gmail.com>
2024-02-09 08:55:25 -0600

    Functioning scripts: ml/model.py, ml/data.py, train_model.py.  Prints Slice_output.txt

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,9 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.linear_model import LogisticRegression  #Best model??
+from .data import process_data, apply_label
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -10,17 +13,18 @@ def train_model(X_train, y_train):
 
     Inputs
     ------
-    X_train : np.array
+    X_train: np.array
         Training data.
-    y_train : np.array
+    y_train: np.array
         Labels.
     Returns
     -------
     model
         Trained machine learning model.
     """
-   # TODO: implement the function
-    pass
+    lr = LogisticRegression(max_iter=1000)
+    model = lr.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
@@ -29,15 +33,15 @@ def compute_model_metrics(y, preds):
 
     Inputs
     ------
-    y : np.array
+    y: np.array
         Known labels, binarized.
-    preds : np.array
+    preds: np.array
         Predicted labels, binarized.
     Returns
     -------
-    precision : float
-    recall : float
-    fbeta : float
+    precision: float
+    recall: float
+    fbeta: float
     """
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
@@ -52,15 +56,14 @@ def inference(model, X):
     ------
     model : ???
         Trained machine learning model.
-    X : np.array
+    X: np.array
         Data used for prediction.
     Returns
     -------
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    return model.predict(X)
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -69,21 +72,25 @@ def save_model(model, path):
     ------
     model
         Trained machine learning model or OneHotEncoder.
-    path : str
+    path: str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    with open(path, 'wb') as file:
+        pickle.dump(model, file)
+    print("ml/model:Pickle save_model Completed")
+    pass 
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
-    # TODO: implement the function
-    pass
+
+    with open(path, 'rb') as pickle_file:
+        content = pickle.load(pickle_file)
+    print("ml/model:load_model Completed")
+    return content
 
 
 def performance_on_categorical_slice(
-    data, column_name, slice_value, categorical_features, label, encoder, lb, model
-):
+    data, column_name, slice_value, categorical_features, label, encoder, lb, model):
     """ Computes the model metrics on a slice of the data specified by a column name and
 
     Processes the data using one hot encoding for the categorical features and a
@@ -92,20 +99,20 @@ def performance_on_categorical_slice(
 
     Inputs
     ------
-    data : pd.DataFrame
+    data: pd.DataFrame
         Dataframe containing the features and label. Columns in `categorical_features`
-    column_name : str
+    column_name: str
         Column containing the sliced feature.
-    slice_value : str, int, float
+    slice_value: str, int, float
         Value of the slice feature.
     categorical_features: list
         List containing the names of the categorical features (default=[])
-    label : str
+    label: str
         Name of the label column in `X`. If None, then an empty array will be returned
         for y (default=None)
-    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+    encoder: sklearn.preprocessing._encoders.OneHotEncoder
         Trained sklearn OneHotEncoder, only used if training=False.
-    lb : sklearn.preprocessing._label.LabelBinarizer
+    lb: sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
     model : ???
         Model used for the task.
@@ -117,12 +124,17 @@ def performance_on_categorical_slice(
     fbeta : float
 
     """
-    # TODO: implement the function
+
+    ##column, column feature, where do these go?
+    data = data[data[column_name] == slice_value] 
     X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
-    )
-    preds = # your code here to get prediction on X_slice using the inference function
+        X = data, 
+        categorical_features = categorical_features, 
+        label = label, 
+        training = False, 
+        encoder = encoder, 
+        lb = lb)
+    
+    preds = inference(model, X_slice) # get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
